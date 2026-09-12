0bfa51ac730791436c0b29b6b14dd8554999635d
sneha121083-dotcom <sneha121083@gmail.com>
2026-09-12 15:20:10 +0530

    Add files via upload

---
+++ two_sum.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class two_sum{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target: ");
+        int target = sc.nextInt();
+
+        // Finding two numbers
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                if (nums[i] + nums[j] == target) {
+                    System.out.println("Indices: [" + i + ", " + j + "]");
+                    return;
+                }
+            }
+        }
+
+        System.out.println("No solution found.");
+
+        sc.close();
+    }
+}
